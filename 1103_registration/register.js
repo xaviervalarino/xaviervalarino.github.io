e43c3392176f9df3b81f746c36c3b375d1db213c
Coffee Cola <xvalarino@gmail.com>
2014-11-06 15:30:18 -0800

    updates
    a lot

--- 1103_registration/register.js
+++ 1103_registration/register.js
@@ -1,41 +1,40 @@
 (function(){
 // DOM Elements
 	var passwordInput = document.getElementById('password');
+	var passConfirmInput = document.getElementById('confirm-password')
 	var uppercase = document.getElementById('uppercase');
 	var lowercase = document.getElementById('lowercase');
 	var digits = document.getElementById('digits');
 	var symbol = document.getElementById('symbol');
 	var length = document.getElementById('length');
+	var formBottom =	document.getElementById('bottom');
+	var li = document.getElementsByClassName('li');
 // Password Requirements
 	var containsUppercase = /[A-Z]/;
 	var containsLowercase = /[a-z]/;
 	var containsDigits = /[0-9]/;
 	var containsSpecialChar = /[!@#$%^&*)(+=._-]+/;
-
+// Password test variables
 	var passwordLength;
 	var includesUppercase;
 	var inludesLowercase;
 	var includesDigits;
 	var includesSpecialChar;
+	var isPasswordValid =	(	passwordLength &&
+								includesUppercase &&
+								inludesLowercase &&
+								includesDigits &&
+								includesSpecialChar );
 
 /* email address must be correct format */
+
 /* -------------\
-	Password Tester
+	Test password on keystroke
 \---------------*/
-		function checkPasswordFormat( test, requirement ) {
-			if (test) {
-				requirement.classList.add("success");
-			//	return true;
-			}else{
-				requirement.classList.remove("success");
-			//	return false;
-		}
-	}
-/* -------------\
-	Test on input event
-\---------------*/
-	passwordInput.addEventListener( 'input', function() {
+	passwordInput.addEventListener( 'input', function () {
+
 		var value = passwordInput.value;
+
 		//test input value against password requirements
 		var lengthTest = value.length >= 8;
 		var uppercaseTest = containsUppercase.test(value);
@@ -49,45 +48,61 @@
 		includesDigits = checkPasswordFormat( digitTest , digits );
 		includesSpecialChar = checkPasswordFormat( specialCharTest , symbol );
 	});
+	/* -------------\
+		Password Tester
+	\---------------*/
+		function checkPasswordFormat( test, requirement ) {
 
+			test ? psswrdReqMet() : removeSuccess();
+			return test;
+
+			 function psswrdReqMet() {
+
+			 	requirement.classList.add('success');
+			 }
+
+			 function removeSuccess() {
+
+			 	var removeSuccess= requirement.classList.remove('success');
+				requirement.className === 'li Success' ? removeSuccessClass : {} ;
+			}			
+	}
+/* -------------\
+	Open and Close requirements drawer
+\---------------*/
+	passwordInput.addEventListener( 'focus', function(){
+		
+		isPasswordValid ? {} : formBottom.classList.add('animate-bottom');
+		
+	});
+	// TODO make this modular so you can use if for Confirm password
+	passwordInput.addEventListener( 'blur', function(){
+
+		isPasswordValid =	(	passwordLength &&
+								includesUppercase &&
+								inludesLowercase &&
+								includesDigits &&
+								includesSpecialChar );
+		isPasswordValid ? (
+			( 
+			formBottom.classList.remove('animate-bottom') ,
+			console.log(isPasswordValid)
+			)
+		) : (
+			errors()
+		);
+	})
+
+	function errors(){
+		for( i = 0; i < li.length; i++ ) {
+			//li[i].className === "li success" ? {} :(
+				li[i].classList.add('error')
+			//);
+			// TODO make this per only the correct li
+		}
+	}
 /* -------------\
 	If password passes test
 \---------------*/
-	// password.blur(function() {
-	// 	if ( !passwordLongEnough || !mixedCase || !includesDigits || !includesSpecialChar ) {
-	// 		$(this).addClass('errorColor');
-	// 	}
-	// 	else {
-	// 		$(this).removeClass('errorColor');
-	// 		$(this).next("ul").slideUp(600);
-	// 		$(this).closest("div").delay(800).removeClass('passwordHelp');
-	// 	}
-	// });//end blur
 
-// 	// If Password field is in focus,
-// 		ul variables
-// 		// 	show psswrdTester
-// 			if psswrdTester !== all success
-// 		// 	else hide psswrdTester
-
-// 	// Password Requirements
-// 		TEST VARIABLES
-// 		// Longer than 8 characters
-// 		// Has uppercase
-// 		// Has lowercase
-// 		// Has a numeric digit
-// 		// Has a special characters
-// 		TEST PASSWORD on keyup
-// 			password.value within function
-// 		// If password value pass (req)
-// 		// 	show success in li
-// 		// 	else remove
-
-// /* Match Confirm Password */
-
-// /* 1st name, last name, company name must have more than 2 characters */
-	
-// 	on.keyupfunction(){
-// 		// test
-// 	}
 })();
